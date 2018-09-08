409dd73053b0ee7f2a9609524dc57928df20484a
Thofeeq <thofeeqm@gmail.com>
2018-09-08 00:39:22 +0000

    adding final touches

--- express-server.js
+++ express-server.js
@@ -1,12 +1,14 @@
 "use strict";
+/////////////////////// Required modules and Globals///////////////////
 const express = require("express");
 let app = express();
 const PORT = 8080; // default port 8080
 const cookieSession = require("cookie-session")
 const bodyParser = require("body-parser");
 const bcrypt = require('bcrypt');
-
-let errorHTML = {
+let testPass = "a";
+let testPass2 = "b";
+let errorHTML = {                       //Keeps track of any input/invalid errors so that it can be displayed on ejs
   emailExistError: false,
   invalidInputError: false,
   invalidCrendential: false,
@@ -28,34 +30,33 @@ const users = {
   "userRandomID": {
     id: "userRandomID", 
     email: "user@example.com", 
-    password: "purp"
+    password: bcrypt.hashSync(testPass,10)
   },
  "user2RandomID": {
     id: "user2RandomID", 
     email: "user2@example.com", 
-    password: "dish"
+    password: bcrypt.hashSync(testPass2,10),
   }
 }
 
-
+//////////////////////////////////////Setup///////////////////////////////////////////////
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use('/style', express.static('style'));
 app.use(cookieSession({
   name: 'session',
   keys: ['development'],
-
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
+///////////////////////////////Helper function///////////////////////////////////////////
 
+//Will return a boolean if a given value exist in an object
 function doesValueExist(object,property,value){
   let arrayOfKeys = [];
   arrayOfKeys = Object.keys(object);
   let isTrue = false;
-  arrayOfKeys.forEach(function(key){
-    // console.log(object[key][property] + "===?" + value);
-    
+  arrayOfKeys.forEach(function(key){   
     if(object[key][property] === value ) {
        isTrue = true;
     }
@@ -63,6 +64,7 @@ function doesValueExist(object,property,value){
 return isTrue;
 }
 
+//will return the id of the user(primary key) given the email (or other property if modified)
 function getID(object,emailValue){
   let arrayOfKeys = Object.keys(object);
   let ID = "";
@@ -73,7 +75,7 @@ function getID(object,emailValue){
   });
 return ID;
 }
-
+//will return the password of the user
 function getPass(object,emailValue){
   let arrayOfKeys = Object.keys(object);
   let password = "";
@@ -84,22 +86,20 @@ function getPass(object,emailValue){
   });
 return password;
 }
-
+//will generate random string given the length (6 in this case);
 function generateRandomString(strLength) {
   
   let outputArray = [];
   let str = "";
   
-    for(var i = 0; i < strLength; i++)
-    {
+    for(var i = 0; i < strLength; i++){
       let randomNum = String.fromCharCode(Math.random() * (122 - 65) + 65);
+      //omitting slashes in the random generated string so it does not interfere with URI 
       if(randomNum === '/' || randomNum === '\\') {
         randomNum = 'x';
       };
       outputArray.push(randomNum);
-    }
-    // console.log(outputArray);
-    
+    }    
     str = outputArray.join('');
     return str;
 }
@@ -119,11 +119,18 @@ function validateURL(urlToTest)
 }
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls");
 });
 app.get("/login", (req, res) => {
-  let templateVars = {user: users[req.session.user_id], error: errorHTML}
-  res.render("login", templateVars);
+  if(!req.session.user_id)
+  {
+    let templateVars = {user: users[req.session.user_id], error: errorHTML}
+    res.render("login", templateVars);
+  }
+  else{
+    res.redirect("/urls");
+  }
+
 });
 
 app.get("/urls.json", (req, res) => {
@@ -132,8 +139,27 @@ app.get("/urls.json", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  let templateVars = {user: users[req.session.user_id], urls: urlDatabase, error: errorHTML};
-  res.render("urls_index", templateVars);
+
+  
+  if(!req.session.user_id){
+    res.redirect("/login");
+  }
+  else
+  {
+    console.log(Object.keys(users).indexOf(req.session.user_id));
+    if(Object.keys(users).indexOf(req.session.user_id) === -1)
+    {
+      req.session = null;
+      return res.redirect('/login');
+    }
+    else
+    {
+      console.log("here!");
+      let templateVars = {user: users[req.session.user_id], urls: urlDatabase, error: errorHTML};
+      res.render("urls_index", templateVars);
+    }
+
+  }
   // console.log(templateVars);
 });
 
@@ -152,8 +178,16 @@ app.get("/hello", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   // let longURL = ...
   // console.log(`${urlDatabase[req.params.shortURL]}`);
-  res.statusCode = 301;
-  res.redirect(`${urlDatabase[req.params.shortURL].url}`);
+  if(urlDatabase[req.params.shortURL])
+  {
+    res.statusCode = 301;
+    res.redirect(`${urlDatabase[req.params.shortURL].url}`);
+  }
+  else{
+    res.statusCode = 404;
+    res.send("<h1>Errpr 404: Page not found!");
+  }
+
 });
 app.get("/register", (req, res) => {
   let templateVars = {user: users[req.session.user_id],shortURL: req.params.id,longURL: urlDatabase[req.params.id],errorCodes: errorHTML };
@@ -161,10 +195,15 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  
+ if(!urlDatabase[req.params.id])
+ {
+   res.send("<h1>Error: 404 - URL Not found! click <a href='/urls'>here</a> to return<h1>");
+ } 
+ else
+ {
   if(req.session.user_id && req.session.user_id=== urlDatabase[req.params.id].urlOwner)
   {
-    let templateVars = { user: users[req.session.user_id],shortURL: req.params.id,longURL: urlDatabase[req.params.id],error: errorHTML };
+    let templateVars = { user: users[req.session.user_id],shortURL: req.params.id,longURL: urlDatabase[req.params.id],error: errorHTML, urls:urlDatabase };
     res.render("urls_show", templateVars);
   }
   else if(req.session.user_id)
@@ -174,6 +213,8 @@ app.get("/urls/:id", (req, res) => {
   else{
     return res.redirect("/login");
   }
+
+ }
 
 });
 
@@ -213,7 +254,7 @@ app.post("/urls/:shortURLID/delete", (req, res) => {
   if(req.session.user_id)
   {
     delete urlDatabase[req.params.shortURLID];
-    res.redirect(`http://localhost:${PORT}/urls`);
+    res.redirect(`/urls`);
   }
   
   
@@ -229,7 +270,9 @@ if(req.body.email && req.body.password){
     users[userID].id = userID;
     users[userID].email = req.body.email;
     users[userID].password = bcrypt.hashSync(req.body.password, 10);
-    return res.redirect("/login");
+    req.session.user_id = getID(users, req.body.email);
+    return res.redirect("/urls");
+
     console.log(users);
   } else {
    
@@ -248,7 +291,7 @@ else{
   // console.log(errorHTML.showError1);
   // console.log(errorHTML.showError2);
   res.statusCode = 400;
-  return res.redirect(`http://localhost:${PORT}/register`);
+  return res.redirect(`/register`);
 }
 
 
@@ -267,18 +310,18 @@ app.post("/login", (req, res) => {
       req.session.user_id = getID(users, req.body.email);
       // cookieStatus.cookieValue = getID(users, req.body.email);
       // console.log(cookieStatus.cookieValue);
-      res.redirect(`http://localhost:${PORT}/urls`)
+      res.redirect("/urls")
       console.log(users);
     }
     else{
       errorHTML.invalidCrendential = true;
       res.statusCode = 403;
-      return res.redirect(`http://localhost:${PORT}/login`);
+      return res.redirect("/login");
     }
   } else {
     errorHTML.invalidCrendential = true
     res.statusCode = 403;
-    return res.redirect(`http://localhost:${PORT}/login`);
+    return res.redirect("/login");
     
   }
 
@@ -287,7 +330,7 @@ app.post("/logout", (req, res) => {
   req.session = null;
   // cookieStatus.cookieValue = "";
   // cookieStatus.isLoggedIn = false;
-  res.redirect(`http://localhost:${PORT}/urls`);
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
